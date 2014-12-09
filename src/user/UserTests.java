d3f3e32225bbeee1417e35b9ac431a2fba14120f
Joel Rollny <rollny@student.chalmers.se>
2014-12-09 16:58:25 +0100

    Modified customer into user instead, plus added some comments.

---
+++ src/user/UserTests.java
@@ -0,0 +1,155 @@
+package user;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * This class simulates a user and contains
+ * both valid and invalid uses of the system (based
+ * on our use cases, among other things). At the moment
+ * the class only contains test stubs for methods a user
+ * of the system is allowed to invoke. 
+ *
+ */
+public class UserTests {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	
+	
+	/**
+	 * Test method for {@link Classes.impl.IBookingManagementImplImpl#updateBooking()}.
+	 */
+	@Test
+	public void testUpdateBooking() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Classes.impl.IBookingManagementImplImpl#getBooking(int)}.
+	 */
+	@Test
+	public void testGetBookingInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Classes.impl.IBookingManagementImplImpl#getBooking(int, java.util.Date)}.
+	 */
+	@Test
+	public void testGetBookingIntDate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Classes.impl.IBookingManagementImplImpl#confirmBooking(int)}.
+	 */
+	@Test
+	public void testConfirmBooking() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Classes.impl.IBookingManagementImplImpl#searchRoom(java.util.Date, java.util.Date, java.lang.Class, int, int, int)}.
+	 */
+	@Test
+	public void testSearchRoom() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Classes.impl.IBookingManagementImplImpl#cancelBooking(int)}.
+	 */
+	@Test
+	public void testCancelBooking() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Classes.impl.IBookingManagementImplImpl#checkIn(java.lang.Class)}.
+	 */
+	@Test
+	public void testCheckIn() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Classes.impl.IBookingManagementImplImpl#checkOut(java.lang.Class)}.
+	 */
+	@Test
+	public void testCheckOut() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Classes.impl.IBookingManagementImplImpl#addCustomerInformationToBooking(int, java.lang.String, java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testAddCustomerInformationToBooking() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Classes.impl.IBookingManagementImplImpl#createPendingBooking(java.util.Date, java.util.Date, int)}.
+	 */
+	@Test
+	public void testCreatePendingBooking() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 *  Test method for {@link Classes.impl.IFinanceImplImpl#calculatePayment(int)}.
+	 */
+	@Test
+	public void testCalculatePayment() {
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 *  Test method for {@link Classes.impl.IFinanceImplImpl#payBill(int)}.
+	 */
+	@Test
+	public void testPayBill() {
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 *  Test method for {@link Classes.impl.IFinanceImplImpl#validateWithBank(java.lang.String, java.lang.String, int, int, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testValidateWithBank() {
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 *  Test method for {@link Classes.impl.IFinanceImplImpl#bankTransfer(java.lang.String, java.lang.String, int, int, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testBankTransfer() {
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 *  Test method for {@link Classes.impl.IFinanceImplImpl#bankSendInvoice()}.
+	 */
+	@Test
+	public void testBankSendInvoice() {
+		fail("Not yet implemented");
+	}
+
+}
