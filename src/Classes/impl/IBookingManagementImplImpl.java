55c984dc3a996184ffd29e2cdff36c7f18e6c730
Joel Rollny <rollny@student.chalmers.se>
2014-12-10 20:26:58 +0100

    uncommented lines in addCustomerInformationToBooking

--- src/Classes/impl/IBookingManagementImplImpl.java
+++ src/Classes/impl/IBookingManagementImplImpl.java
@@ -212,11 +212,10 @@ public class IBookingManagementImplImpl extends MinimalEObjectImpl.Container imp
 	 * @generated NOT
 	 */
 	public synchronized boolean addCustomerInformationToBooking(int bookingID, String firstName, String lastName, String email, String ph) {
-		// Need to regenerate model to get firstname, lastname, email or phonenumber variables in BookingImpl
-		// pendingBookings.get(bookingID).setFirstName(firstName);
-		// pendingBookings.get(bookingID).setLastName(lastName);
-		// pendingBookings.get(bookingID).setEmail(email);
-		// pendingBookings.get(bookingID).setPhoneNumber(ph);
+		pendingBookings.get(bookingID).setFirstName(firstName);
+		pendingBookings.get(bookingID).setLastName(lastName);
+		pendingBookings.get(bookingID).setEmail(email);
+		pendingBookings.get(bookingID).setPhoneNumber(ph);
 		return true;
 	}
 
